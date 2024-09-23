ef71cba9dc7062148e1e9cf0e8d2bd1e2563b01a
nicolezeg <nzegarra07@gmail.com>
2024-09-23 10:41:15 -0500

    program

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -7,14 +7,14 @@ class Program
         Job job1 = new Job();
         job1._jobTitle = "Software Engineer";
         job1._company = "Microsoft";
-        job1._startYear = 2019;
-        job1._endYear = 2022;
+        job1._startYear = 2018;
+        job1._endYear = 2023;
 
         Job job2 = new Job();
         job2._jobTitle = "Manager";
         job2._company = "Apple";
-        job2._startYear = 2022;
-        job2._endYear = 2023;
+        job2._startYear = 2023;
+        job2._endYear = 2024;
 
         //job1.Display();
         //job2.Display();
