9ef80669f5e2409923f9c249edb8b5592df1d690
nicolezeg <nzegarra07@gmail.com>
2024-09-28 23:46:33 -0500

    program

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -10,7 +10,7 @@ class Program
         while (true)
         {
             Console.WriteLine("Welcome to the Journal Program!");
-            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("Please select one of the following:");
             Console.WriteLine("1. Write a new entry");
             Console.WriteLine("2. Display the journal");
             Console.WriteLine("3. Save the journal to a file");
