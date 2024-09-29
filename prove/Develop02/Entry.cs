894e9fd5c514b465fbc545f9378b449545716ad4
nicolezeg <nzegarra07@gmail.com>
2024-09-28 23:40:13 -0500

    entry

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,13 +1,13 @@
 using System;
 public class Entry 
 {
-    public string _dates;
+    public string _date;
     public string _promptText;
     public string _entryText;
 
     public void Display()
     {
-        Console.WriteLine($"Date: {_dates} - Prompt: {_promptText}");
+        Console.WriteLine($"Date: {_date} - Prompt: {_promptText}");
         Console.WriteLine($"{_entryText}");
         Console.WriteLine();
     }
